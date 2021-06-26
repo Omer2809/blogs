049050947dd6be19e732adf91c5366e1631ff10c
Mohd Omer <omer786501@gmail.com>
2021-06-26 11:59:33 +0530

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Omer Blogs`,
     author: {
-      name: `Kyle Mathews`,
+      name: `Mohd Omer`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
